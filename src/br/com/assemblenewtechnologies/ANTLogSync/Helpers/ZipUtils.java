8a23f01ba109a09337da42d0813ece18366a8ac2
MC LOC <mcl.devel@gmail.com>
2021-04-15 09:50:08 -0300

    initial archive implementation

---
+++ src/br/com/assemblenewtechnologies/ANTLogSync/Helpers/ZipUtils.java
@@ -0,0 +1,117 @@
+package br.com.assemblenewtechnologies.ANTLogSync.Helpers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipUtils {
+
+  private List <String> fileList;
+  private static String OUTPUT_ZIP_FILE = null; //"Folder.zip";
+  private static String SOURCE_FOLDER = null; //"D:\\Reports"; // SourceFolder path
+
+  public ZipUtils() {
+      fileList = new ArrayList < String > ();
+  }
+
+  public static void main(String[] args) {
+      ZipUtils appZip = new ZipUtils();
+      appZip.generateFileList(new File(SOURCE_FOLDER));
+      appZip.zipIt(OUTPUT_ZIP_FILE);
+  }
+
+  public void zipIt(String zipFile) {
+      byte[] buffer = new byte[1024];
+      String source = new File(SOURCE_FOLDER).getName();
+      FileOutputStream fos = null;
+      ZipOutputStream zos = null;
+      try {
+          fos = new FileOutputStream(zipFile);
+          zos = new ZipOutputStream(fos);
+
+          System.out.println("Output to Zip : " + zipFile);
+          FileInputStream in = null;
+
+          for (String file: this.fileList) {
+              System.out.println("File Added : " + file);
+              ZipEntry ze = new ZipEntry(source + File.separator + file);
+              zos.putNextEntry(ze);
+              try {
+                  in = new FileInputStream(SOURCE_FOLDER + File.separator + file);
+                  int len;
+                  while ((len = in .read(buffer)) > 0) {
+                      zos.write(buffer, 0, len);
+                  }
+              } finally {
+                  in.close();
+              }
+          }
+
+          zos.closeEntry();
+          System.out.println("Folder successfully compressed");
+
+      } catch (IOException ex) {
+          ex.printStackTrace();
+      } finally {
+          try {
+              zos.close();
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+      }
+  }
+
+  public void generateFileList(File node) {
+      // add file only
+      if (node.isFile()) {
+          fileList.add(generateZipEntry(node.toString()));
+      }
+
+      if (node.isDirectory()) {
+          String[] subNote = node.list();
+          for (String filename: subNote) {
+              generateFileList(new File(node, filename));
+          }
+      }
+  }
+
+  private String generateZipEntry(String file) {
+      return file.substring(SOURCE_FOLDER.length() + 1, file.length());
+  }
+
+/**
+ * @return the oUTPUT_ZIP_FILE
+ */
+public static String getOUTPUT_ZIP_FILE() {
+	return OUTPUT_ZIP_FILE;
+}
+
+/**
+ * @param oUTPUT_ZIP_FILE the oUTPUT_ZIP_FILE to set
+ */
+public static void setOUTPUT_ZIP_FILE(String oUTPUT_ZIP_FILE) {
+	OUTPUT_ZIP_FILE = oUTPUT_ZIP_FILE;
+}
+
+/**
+ * @return the sOURCE_FOLDER
+ */
+public static String getSOURCE_FOLDER() {
+	return SOURCE_FOLDER;
+}
+
+/**
+ * @param sOURCE_FOLDER the sOURCE_FOLDER to set
+ */
+public static void setSOURCE_FOLDER(String sOURCE_FOLDER) {
+	SOURCE_FOLDER = sOURCE_FOLDER;
+}
+  
+  
+  
+}
--- src/br/com/assemblenewtechnologies/ANTLogSync/Main.java
+++ src/br/com/assemblenewtechnologies/ANTLogSync/Main.java
@@ -4,10 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Arrays;
 
 import org.postgresql.copy.CopyManager;
@@ -15,10 +16,13 @@ import org.postgresql.core.BaseConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import br.com.assemblenewtechnologies.ANTLogSync.Helpers.ZipUtils;
 import br.com.assemblenewtechnologies.ANTLogSync.jdbc.JDBCConnection;
 
 public class Main {
-	private final static String RTD_DIRETCTORY = "/home/mcloc/Downloads/18";
+	private final static String RTD_DIRETCTORY = "/ANT-TOOLCHAIN/ANTOption/data_load/";
+	private final static String ARCHIVE_DIRETCTORY = "/ANT-TOOLCHAIN/ANTOption/archive/";
+	private final static String ARCHIVE_BUFFER_DIRETCTORY = "/ANT-TOOLCHAIN/ANTOption/archive_buffer/";
 	private static Logger LOGGER = LoggerFactory.getLogger(Main.class);
 	private static GlobalProperties globalProperties = new GlobalProperties();
 	private static JDBCConnection jdbcConnection;
@@ -29,18 +33,20 @@ public class Main {
 	private static long timer3;
 	private static long timer4;
 	private static long timer5;
-	
+
 	private static int files_processed = 0;
 	private static int directories_processed = 0;
 	private static long rows_processed = 0;
-	
+
+	private static String current_directory;
+	private static String file_separator;
 
 	public static void main(String[] args) throws Exception {
+		file_separator = java.nio.file.FileSystems.getDefault().getSeparator();
 		start_time = System.currentTimeMillis();
 		LOGGER.info("Initializing ANTLogSync...");
 		LOGGER.info("Directory to search RTD files: ." + RTD_DIRETCTORY);
-		
-		
+
 		try {
 			jdbcConnection = new JDBCConnection(globalProperties);
 		} catch (SQLException e) {
@@ -50,77 +56,104 @@ public class Main {
 
 		connection = jdbcConnection.getConn();
 		connection.setAutoCommit(true);
-		
+
 		start_time = System.currentTimeMillis();
-		
+
 		File dir = new File(RTD_DIRETCTORY);
 		File[] list = dir.listFiles();
 		Arrays.sort(list);
 		processFiles(list);
 		timer1 = System.currentTimeMillis();
 		long total_time = (timer1 - start_time) / 1000;
-		
+
 		jdbcConnection.connClose();
 		LOGGER.info("Total time to process: " + total_time + " seconds");
 		LOGGER.info("Total directories processed: " + directories_processed);
 		LOGGER.info("Total files processed on all directories: " + files_processed);
 		LOGGER.info("Total records processed on all directories: " + rows_processed);
-		LOGGER.info("Files per sec: " + (files_processed/total_time));
-		LOGGER.info("Records per sec: " + (rows_processed/total_time));
+		LOGGER.info("Files per sec: " + (files_processed / total_time));
+		LOGGER.info("Records per sec: " + (rows_processed / total_time));
 
 	}
-	
-	private static void processFiles(File[] files) throws Exception {
-        for (File file : files) {
-            if (file.isDirectory()) {
-            	directories_processed++;
-                LOGGER.info("Processing Directory: " + file.getAbsolutePath());
-                File[] _list = file.listFiles();
-                Arrays.sort(_list);
-                processFiles(_list); // Calls same method again.
-            } else {
-            	files_processed++;
-//                LOGGER.info("Loading File: " + file.getAbsolutePath());
-                loadCSV(file.getAbsolutePath());
-            }
-        }
-    }
 
-	private static void loadCSV(String absolutePath) throws Exception {
+	private static void processFiles(File[] files) throws Exception {
+		String last_directory = null;
+		for (File file : files) {
+			if (file.isDirectory()) {
+				current_directory = file.getName();
+				if (last_directory != null && last_directory != current_directory) {
+					zipArchive(last_directory);
+				}
+
+				directories_processed++;
+				LOGGER.info("Processing Directory: " + file.getAbsolutePath());
+				File[] _list = file.listFiles();
+				Arrays.sort(_list);
+				processFiles(_list); // Calls same method again.
+				last_directory = current_directory;
+			} else {
+				files_processed++;
+//                LOGGER.info("Loading File: " + file.getAbsolutePath());
+				loadCSV(file);
+			}
+		}
+	}
+
+	private static void zipArchive(String last_directory) {
+		String zip_archive_path = ARCHIVE_BUFFER_DIRETCTORY + file_separator + last_directory;
+		String zip_file = ARCHIVE_DIRETCTORY+file_separator + last_directory + ".zip";
+		ZipUtils appZip = new ZipUtils();
+		appZip.setOUTPUT_ZIP_FILE(zip_file);
+		appZip.setSOURCE_FOLDER(zip_archive_path);
+		appZip.generateFileList(new File(zip_archive_path));
+		appZip.zipIt(zip_file);
+
+	}
+
+	private static void loadCSV(File file) throws Exception {
 		try {
-		    long rowsInserted = new CopyManager((BaseConnection) connection)
-		            .copyIn(
-		                "COPY B3Log.B3SignalLogger "+
-		            "( "
-		            + "asset,"
-		            + "data,"
-		            + "hora,"
-		            + "ultimo,"
-		            + "strike,"
-		            + "negocios,"
-		            + "quantidade,"
-		            + "volume,"
-		            + "oferta_compra,"
-		            + "oferta_venda,"
-		            + "VOC,"
-		            + "VOV,"
-		            + "vencimento,"
-		            + "validade,"
-		            + "contratos_abertos,"
-		            + "estado_atual,"
-		            + "relogio"
-		            + ") "
-		            + "FROM STDIN (FORMAT csv, HEADER true, DELIMITER ',')", 
-		                new BufferedReader(new FileReader(absolutePath))
-		                );
+			long rowsInserted = new CopyManager((BaseConnection) connection).copyIn(
+					"COPY B3Log.B3SignalLogger " + "( " + "asset," + "data," + "hora," + "ultimo," + "strike,"
+							+ "negocios," + "quantidade," + "volume," + "oferta_compra," + "oferta_venda," + "VOC,"
+							+ "VOV," + "vencimento," + "validade," + "contratos_abertos," + "estado_atual," + "relogio"
+							+ ") " + "FROM STDIN (FORMAT csv, HEADER true, DELIMITER ',')",
+					new BufferedReader(new FileReader(file.getAbsoluteFile())));
 //		    LOGGER.info("File: " + absolutePath + " LOADED: " + rowsInserted + " rows");
-		    rows_processed += rowsInserted;
-		} catch (SQLException | IOException e) {
-			LOGGER.error("Error Loading File: " + absolutePath);
-			LOGGER.error(e.getMessage(), e);
-			throw e;
+			rows_processed += rowsInserted;
+			archiveFile(file);
+
+		} catch (SQLException e) {
+			LOGGER.error("Database Error Loading File: " + file.getAbsoluteFile());
+			LOGGER.error(e.getMessage());
+			LOGGER.error("Archiving File: " + file.getAbsoluteFile());
+//			LOGGER.error(e.getMessage(), e);
+
+			// throw e;
+		} catch (IOException e) {
+			LOGGER.error("Error IO Loading File: " + file.getAbsoluteFile());
+			LOGGER.error(e.getMessage());
 		}
 
+	}
+
+	private static void archiveFile(File file) throws IOException {
+		String archive_path = ARCHIVE_BUFFER_DIRETCTORY + file_separator + current_directory;
+		File f = new File(archive_path);
+		if (!f.exists()) {
+			f.mkdir();
+		}
+
+		file.renameTo(
+				new File(ARCHIVE_DIRETCTORY + file_separator + current_directory + file_separator + file.getName()));
+
+		try {
+			Files.move(Paths.get(file.getAbsolutePath()), Paths.get(archive_path + file_separator + file.getName()),
+					StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			LOGGER.error("Error IO Archiving File: " + file.getAbsoluteFile());
+			LOGGER.error(e.getMessage());
+			throw e;
+		}
 	}
 
 }
