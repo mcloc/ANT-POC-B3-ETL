d0d8cefb8b2dba0bcae4a1c81bb0e33409a69141
MC LOC <mcl.devel@gmail.com>
2021-05-20 16:23:28 -0300

    clean up ETL, implemented ETL Handler, rotine ETL is ON

---
+++ src/br/com/assemblenewtechnologies/ANTLogSync/process_handlers/ETLHandler.java
@@ -0,0 +1,41 @@
+package br.com.assemblenewtechnologies.ANTLogSync.process_handlers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.assemblenewtechnologies.ANTLogSync.rotines.Etl;
+
+public class ETLHandler implements Runnable {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(ETLHandler.class);
+	private Etl etl_rotine;
+
+	public ETLHandler(Etl etl) throws Exception {
+		etl_rotine = etl;
+	}
+
+	@Override
+	public void run() {
+		try {
+			LOGGER.info("[ETL Thread] Starting...");
+			while (true) {
+				try {
+					etl_rotine.setStartTime();
+					etl_rotine.etl1_populate_assets();
+					etl_rotine.setExecuting(false);
+				} catch (Exception e) {
+					LOGGER.error("[ETL Thread] Error");
+					LOGGER.error(e.getMessage());
+					e.printStackTrace();
+				}
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			LOGGER.error(e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+}
--- src/br/com/assemblenewtechnologies/ANTLogSync/rotines/Etl.java
+++ src/br/com/assemblenewtechnologies/ANTLogSync/rotines/Etl.java
@@ -17,9 +17,15 @@ import org.slf4j.LoggerFactory;
 
 import br.com.assemblenewtechnologies.ANTLogSync.Helpers.DBConnectionHelper;
 import br.com.assemblenewtechnologies.ANTLogSync.model.CsvLoadLot;
+import br.com.assemblenewtechnologies.ANTLogSync.process_handlers.CSVHandler;
+import br.com.assemblenewtechnologies.ANTLogSync.process_handlers.ETLHandler;
 
 public class Etl extends AbstractRotine {
 	private static Logger LOGGER = LoggerFactory.getLogger(Etl.class);
+	private long start_time;
+	private String thread_name = "ETL_HANDLER";
+	private Thread thread;
+	private ETLHandler runnable;
 
 	public void etl0_sanity_check() throws Exception {
 		// verificar se DATA RELOGIO BATE COM DATA LOTE isso resolve sanity de
@@ -36,19 +42,13 @@ public class Etl extends AbstractRotine {
 		LOGGER.info("[ETL] etl0_sanity_check...");
 	}
 
-	private void etl1_populate_assets() throws Exception {
-		LOGGER.info("[ETL]etl1_populate_assets ...");
-
-//		if (1 == 1)
-//			return; // DEBUG
-
+	public void etl1_populate_assets() throws Exception {
 		long start_time = System.currentTimeMillis();
-		LOGGER.info("Initializing B3 SignalLogger ETL phase 1 - 'etl1_populate_assets' ...");
+		LOGGER.debug("Initializing B3 SignalLogger ETL phase 1 - 'etl1_populate_assets' ...");
 
-		// extract group by all assets to assets table
-		// PL SQL
 		Connection connection;
-
+		Statement stmt;
+		ResultSet rs;
 		try {
 			connection = DBConnectionHelper.getNewConn();
 		} catch (SQLException e) {
@@ -56,28 +56,14 @@ public class Etl extends AbstractRotine {
 			throw new Exception("No database connection...");
 		}
 
-		String status_finished = CsvLoadLot.getFinishedStatusStringCommaSeparated();
-		Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-		ResultSet rs = stmt.executeQuery(
-				"select * from Intellect.csv_load_lot where status in (" + status_finished + ") order by lot_name ASC");
-
-		List<BigDecimal> csv_lot_finished = new ArrayList<BigDecimal>();
-		while (rs.next()) {
-			csv_lot_finished.add(rs.getBigDecimal("id"));
-		}
-
-		stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-		rs = stmt.executeQuery("select * from B3Log.B3AtivosOpcoes");
-
-		Map<String, String> ativo_opcoes_db = new HashMap<String, String>();
-		while (rs.next()) {
-			ativo_opcoes_db.put(rs.getString("ativo"), rs.getString("opcao_ativo"));
-		}
+		List<BigDecimal> csv_lot_finished = getFinishedLots(connection);
+		Map<String, String> ativo_opcoes_db = getAssetsInDB(connection);
 
 		// FOR EACH FINISHED LOT
 		for (BigDecimal csv_lot_id : csv_lot_finished) {
+			CsvLoadLot csv_lot = CsvLoadLot.getLotByLotId(csv_lot_id);
 			try {
-				LOGGER.info("Fetching assets from B3Log.B3SignalLoggerRaw:");
+				LOGGER.debug("Fetching assets from B3Log.B3SignalLoggerRaw:");
 				stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 				rs = stmt.executeQuery("select asset, substring(asset, '[A-Z]+') as substr_ativo \n"
 						+ "from B3Log.B3SignalLoggerRaw  \n" + "WHERE strike = 0\n" + "and lot_id = " + csv_lot_id
@@ -86,50 +72,23 @@ public class Etl extends AbstractRotine {
 				if (rs.last()) {
 					rows = rs.getRow();
 					rs.beforeFirst();
-					LOGGER.info("total assets found " + rows);
+					LOGGER.debug("total assets found " + rows + " for this lot: " + csv_lot.getLot_name());
 
 					if (rows == 0)
 						return;
 				}
-
+				List<String> _inserted_ativo = new ArrayList<String>();
 				Map<String, String> _ativos = new HashMap<String, String>();
-//				PreparedStatement preparedStatement;
 				while (rs.next()) {
 
 					if (rs.getString("asset").equals("B3SA3"))
 						continue;
 
-					// Asset and Option already on DB
+					// Assets and Option already on DB
 					if (ativo_opcoes_db.containsKey(rs.getString("asset")))
 						continue;
 					
-					
 					_ativos.put(rs.getString("asset"), rs.getString("substr_ativo"));
-					
-
-//					Statement stmt2 = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-//							ResultSet.CONCUR_READ_ONLY);
-//					String sql = "INSERT INTO B3Log.B3AtivosOpcoes(\n" + "ativo,substr_opcao_ativo,opcao_ativo)\n"
-//							+ "select '" + rs.getString("asset") + "' as ativo, '" + rs.getString("substr_ativo")
-//							+ "' as substr_ativo, asset as opcao_ativo \n" + "from B3Log.B3SignalLoggerRaw  \n"
-//							+ "WHERE lot_id = " + csv_lot_id + " AND strike != 0 AND asset like '"
-//							+ rs.getString("substr_ativo") + "%' \n" + "group by 1,2,3\n" + "order by 1,3";
-////					LOGGER.info(sql);
-//					stmt2.execute(sql);
-//					ResultSet rs2 = stmt2.executeQuery(sql);
-//
-//					while (rs2.next()) {
-//						String compiledQuery = "INSERT INTO B3Log.B3AtivosOpcoes("
-//								+ "ativo,substr_opcao_ativo,opcao_ativo) VALUES" + "(?, ?, ?)";
-//						preparedStatement = connection.prepareStatement(compiledQuery);
-//						preparedStatement.setString(1, rs2.getString("ativo"));
-//						preparedStatement.setString(2, rs2.getString("substr_ativo"));
-//						preparedStatement.setString(3, rs2.getString("opcao_ativo"));
-//						preparedStatement.execute();
-//						preparedStatement.clearParameters();
-////						preparedStatement.close();
-//					}
-
 				}
 
 				stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
@@ -147,9 +106,7 @@ public class Etl extends AbstractRotine {
 				}
 				
 				List<String> _ativos_options = new ArrayList<String>();
-//				PreparedStatement preparedStatement;
 				while (rs.next()) {
-
 					if (rs.getString("opcao").equals("B3SA3"))
 						continue;
 
@@ -157,56 +114,88 @@ public class Etl extends AbstractRotine {
 					if (ativo_opcoes_db.containsKey(rs.getString("opcao")))
 						continue;
 					
-					
 					_ativos_options.add(rs.getString("opcao"));
 				}
 				
 				Iterator<Map.Entry<String, String>> itr = _ativos.entrySet().iterator();
-		         
 		        while(itr.hasNext())
 		        {
 		             Map.Entry<String, String> entry = itr.next();
 		             String _ativo = entry.getKey();
 		             String _ativo_substr = entry.getValue();
-		             PreparedStatement preparedStatement;
-		             for(String _option : _ativos_options) {
-		            	 if(_option.contains(_ativo_substr)) {
-		            		 
-		            			String sql = "INSERT INTO B3Log.B3AtivosOpcoes(\n" + "ativo,substr_opcao_ativo,opcao_ativo)\n"
-		    							+ "VALUES (?,?,?)";
-//		    					LOGGER.info(sql);
-		    							
-							preparedStatement = connection.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-							preparedStatement.setString(1, _ativo);
-							preparedStatement.setString(2, _ativo_substr);
-							preparedStatement.setString(3, _option);
-							preparedStatement.execute();
-		            	 }
-		             }
+		             insertNewAssetsInDB(connection, _inserted_ativo, _ativos_options, _ativo, _ativo_substr);
 		        }
-				
-				
-//				connection.close();
+		        
+		        //TODO: INSERT ASSETS WITH NO DERIVATIVES (strike = 0 and none != 0) they are missing
+//		        itr = _ativos.entrySet().iterator();
+//		        while(itr.hasNext())
+//		        {
+//		        	if(_inserted_ativo.add(_ativo);)
+//		        }
 			} catch (Exception e) {
 				LOGGER.error(e.getMessage());
-//				connection.close();
 				// e.printStackTrace();
 				throw e;
 			}
 
-			CsvLoadLot csv_lot = CsvLoadLot.getLotByLotId(csv_lot_id);
+			//INCREMENT CSV LOT STATUS FINISHED TO +1 
 			int csv_status = csv_lot.getStatus();
 			if (csv_status < 0)
 				csv_lot.changeStatus(csv_lot.getStatus() - 1);
 			else
 				csv_lot.changeStatus(csv_lot.getStatus() + 1);
 
-		}
+		} // END LOOP CSV FINISHED LOTS
 
 		long _now = System.currentTimeMillis();
 		long _diff_time = _now - start_time;
-		LOGGER.info("Total time to populate assets: " + _diff_time + "ms ");
+		LOGGER.debug("Total time to populate assets: " + _diff_time + "ms ");
+	}
 
+	private void insertNewAssetsInDB(Connection connection, List<String> _inserted_ativo, List<String> _ativos_options,
+			String _ativo, String _ativo_substr) throws SQLException {
+		PreparedStatement preparedStatement;
+		 for(String _option : _ativos_options) {
+			 if(_option.contains(_ativo_substr)) {
+				 
+					String sql = "INSERT INTO B3Log.B3AtivosOpcoes(\n" + "ativo,substr_opcao_ativo,opcao_ativo)\n"
+							+ "VALUES (?,?,?)";
+//		    					LOGGER.info(sql);
+							
+				preparedStatement = connection.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+				preparedStatement.setString(1, _ativo);
+				preparedStatement.setString(2, _ativo_substr);
+				preparedStatement.setString(3, _option);
+				preparedStatement.execute();
+				 _inserted_ativo.add(_ativo);
+			 }
+		 }
+	}
+
+	private Map<String, String> getAssetsInDB(Connection connection) throws SQLException {
+		Statement stmt;
+		ResultSet rs;
+		//GET ASSESTS ALREADY IN DB
+		stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		rs = stmt.executeQuery("select * from B3Log.B3AtivosOpcoes");
+		Map<String, String> ativo_opcoes_db = new HashMap<String, String>();
+		while (rs.next()) {
+			ativo_opcoes_db.put(rs.getString("ativo"), rs.getString("opcao_ativo"));
+		}
+		return ativo_opcoes_db;
+	}
+
+	private List<BigDecimal> getFinishedLots(Connection connection) throws SQLException {
+		//GET ALL LOTs STATUS FINISHED TO INITIATE ETL PHASE 1 
+		String status_finished = CsvLoadLot.getFinishedStatusStringCommaSeparated();
+		Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		ResultSet rs = stmt.executeQuery(
+				"select * from Intellect.csv_load_lot where status in (" + status_finished + ") order by lot_name ASC");
+		List<BigDecimal> csv_lot_finished = new ArrayList<BigDecimal>();
+		while (rs.next()) {
+			csv_lot_finished.add(rs.getBigDecimal("id"));
+		}
+		return csv_lot_finished;
 	}
 
 	public void etl1_normalization() throws Exception {
@@ -230,18 +219,18 @@ public class Etl extends AbstractRotine {
 
 	@Override
 	public void handler_start() throws Exception {
-		try {
-			etl1_populate_assets();
-		} catch (Exception e) {
-			LOGGER.error(e.getMessage());
-			throw e;
-		}
-
+		runnable = new ETLHandler(this);
+		thread = new Thread(runnable, thread_name );
+		thread.start();
 	}
 
 	@Override
 	public void handler_finish() throws Exception {
-		// TODO Auto-generated method stub
+		thread.interrupt();
+	}
 
+	public void setStartTime() {
+		this.start_time = System.currentTimeMillis();
+		
 	}
 }
